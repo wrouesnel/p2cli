e6967775528e8b0283316227fbd6a8f556bf05cc
Naoto Gohko <naoto-gohko@plastic-machine.red>
2019-01-29 12:59:08 +0900

    fix pongo2.Error: Since the structure of pongo2.Error was changed, I changed Source to ErrorMsg => Filename

--- custom_filters.go
+++ custom_filters.go
@@ -25,23 +25,35 @@ func filterWriteFile(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pon
 	if !in.IsString() {
 		return nil, &pongo2.Error{
 			Sender:   "filter:write_file",
-			ErrorMsg: "Filter input must be of type 'string'.",
+			Filename: "Filter input must be of type 'string'.",
 		}
+		//return nil, &pongo2.Error{
+		//	Sender:   "filter:write_file",
+		//	ErrorMsg: "Filter input must be of type 'string'.",
+		//}
 	}
 
 	if !param.IsString() {
 		return nil, &pongo2.Error{
 			Sender:   "filter:write_file",
-			ErrorMsg: "Filter parameter must be of type 'string'.",
+			Filename: "Filter parameter must be of type 'string'.",
 		}
+		//return nil, &pongo2.Error{
+		//	Sender:   "filter:write_file",
+		//	ErrorMsg: "Filter parameter must be of type 'string'.",
+		//}
 	}
 
 	f, err := os.OpenFile(param.String(), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, os.FileMode(0777))
 	if err != nil {
 		return nil, &pongo2.Error{
 			Sender:   "filter:write_file",
-			ErrorMsg: fmt.Sprintf("Could not open file for output: %s", err.Error()),
+			Filename: fmt.Sprintf("Could not open file for output: %s", err.Error()),
 		}
+		//return nil, &pongo2.Error{
+		//	Sender:   "filter:write_file",
+		//	ErrorMsg: fmt.Sprintf("Could not open file for output: %s", err.Error()),
+		//}
 	}
 	defer f.Close()
 
@@ -49,8 +61,12 @@ func filterWriteFile(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pon
 	if werr != nil {
 		return nil, &pongo2.Error{
 			Sender:   "filter:write_file",
-			ErrorMsg: fmt.Sprintf("Could not write file for output: %s", werr.Error()),
+			Filename: fmt.Sprintf("Could not write file for output: %s", werr.Error()),
 		}
+		//return nil, &pongo2.Error{
+		//	Sender:   "filter:write_file",
+		//	ErrorMsg: fmt.Sprintf("Could not write file for output: %s", werr.Error()),
+		//}
 	}
 
 	return in, nil
@@ -62,16 +78,24 @@ func filterMakeDirs(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pong
 	if !param.IsString() {
 		return nil, &pongo2.Error{
 			Sender:   "filter:make_dirs",
-			ErrorMsg: "Filter parameter must be of type 'string'.",
+			Filename: "Filter parameter must be of type 'string'.",
 		}
+		//return nil, &pongo2.Error{
+		//	Sender:   "filter:make_dirs",
+		//	ErrorMsg: "Filter parameter must be of type 'string'.",
+		//}
 	}
 
 	err := os.MkdirAll(param.String(), os.FileMode(0777))
 	if err != nil {
 		return nil, &pongo2.Error{
 			Sender:   "filter:make_dirs",
-			ErrorMsg: fmt.Sprintf("Could not create directories: %s %s", in.String(), err.Error()),
+			Filename: fmt.Sprintf("Could not create directories: %s %s", in.String(), err.Error()),
 		}
+		//return nil, &pongo2.Error{
+		//	Sender:   "filter:make_dirs",
+		//	ErrorMsg: fmt.Sprintf("Could not create directories: %s %s", in.String(), err.Error()),
+		//}
 	}
 
 	return in, nil
